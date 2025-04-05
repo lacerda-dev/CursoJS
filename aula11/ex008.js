40fafe47ce5246fcfdc4f405b555e5fe9433816d
Matheus Lacerda <mlacerda.work@gmail.com>
2025-04-04 21:30:36 -0300

    primeira aula de condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 67
+var lim = 60
+var dif = vel - lim
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h por hora`)
+
+if(vel>lim){ //Condição Simples
+
+    console.log(`Você ultrapassou a velocidade ${dif}Km/h do limite máximo. VOCÊ FOI MULTADO`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
