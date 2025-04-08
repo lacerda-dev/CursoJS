06fdb782588c4f145479414339fd683fceadfe7a
Matheus Lacerda <matheuslacerda.me@gmail.com>
2025-04-08 14:56:05 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,7 +5,7 @@ num.sort()
 num.push(1)
 console.log(num)
 console.log(`Nosso array tem ${num.length} posições`)
-console.log(`O primeiro valor do meu array é ${num[3]}`)
+console.log(`O primeiro valor do meu array é ${num[0]}`)
 let pos = num.indexOf(8)
 console.log(`O valor 8 está na posição ${pos}`)
 
